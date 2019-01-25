301354a457c8d632bbf86935f29e666efff89eea
Jacob Walser <jwalser90@gmail.com>
2019-01-25 14:32:10 -0500

    examples: remove unnecessary includes

--- examples/ping1d-advanced/ping1d-advanced.ino
+++ examples/ping1d-advanced/ping1d-advanced.ino
@@ -9,8 +9,6 @@
  *  Communication is performed with a Blue Robotics Ping1D Echosounder
  */
 
-#include "pingmessage_all.h"
-#include "ping_parser.h"
 #include "ping1d.h"
 
 #include "SoftwareSerial.h"
